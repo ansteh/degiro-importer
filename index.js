6a511d3b423fd39e795241d54bed788f6415ff6c
ansteh <stehle.andre@googlemail.com>
2018-11-23 21:50:16 +0100

    parse transaction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const _     = require('lodash');
 const fs    = require('fs');
-const parse = require('csv-parse/lib/sync');
+const parse = require('csv-parse');
 
 const { loadFileContent } = require('./util.js');
 
@@ -18,11 +18,86 @@ const preparePortfolio = (entries) => {
 
 const getPortfolio = (filepath) => {
   return loadFileContent(filepath)
-    .then(content => parse(content, { columns: true }))
+    .then(parseContent({ columns: true }))
     .then(preparePortfolio)
 };
 
+const parseContent = _.curry((options, content) => {
+  return new Promise((resolve, reject) => {
+    parse(content, options, (err, output) => {
+      err ? reject(err) : resolve(output);
+    })
+  });
+});
+
+const TRANSACTION_COLUMNS = [
+  'date',
+  'time',
+  'title',
+  'ISIN',
+  'exchange',
+  'amount',
+  'local_share_price_currency',
+  'local_share_price',
+  'local_total_price_currency',
+  'local_total_price',
+  'target_total_price_after_fee_currency',
+  'target_total_price_after_fee',
+  'exchange_rate',
+  'exchange_fee_currency',
+  'exchange_fee',
+  'target_total_price_currency',
+  'target_total_price',
+];
+
+const TRANSACTION_FLOATS = [
+  'local_share_price',
+  'local_total_price',
+  'target_total_price_after_fee',
+  'exchange_rate',
+  'exchange_fee',
+  'target_total_price',
+];
+
+const getTransactions = (filepath) => {
+  return loadFileContent(filepath)
+    .then(parseContent({ columns: TRANSACTION_COLUMNS }))
+    .then(prepareTransactions)
+};
+
+const prepareTransactions = (entries) => {
+  return _.map(entries, (entry) => {
+    entry.amount = parseInt(_.get(entry, 'amount', 0));
+
+    TRANSACTION_FLOATS.forEach((property) => {
+      entry[property] = parseFloat(_.get(entry, property).replace(',', '.'));
+    });
+
+    return entry;
+  });
+};
+
+const explore = (filepath, prepare) => {
+  loadFileContent(filepath)
+    .then((content) => {
+      return new Promise((resolve, reject) => {
+        parse(content, { columns: true }, (err, output) => {
+          err ? reject(err) : resolve(output);
+        })
+      });
+    })
+    .then((entries) => {
+      return prepare ? prepare(entries) : entries;
+    })
+    .then(console.log)
+    .catch(console.log);
+};
+
 module.exports = {
+  explore,
+
   getPortfolio,
   preparePortfolio,
+
+  getTransactions,
 };
--- test.js
+++ test.js
@@ -1,22 +1,14 @@
 const Degiro = require('./index.js');
 
-const filepath = `${__dirname}/resources/Portfolio.csv`;
+// Degiro.explore(`${__dirname}/resources/Portfolio.csv`);
 
-const test = (filepath, prepare) => {
-  loadFileContent(filepath)
-    .then((content) => {
-      const records = parse(content, { columns: true });
-      return records;
-    })
-    .then((entries) => {
-      return prepare ? prepare(entries) : entries;
-    })
-    .then(console.log)
-    .catch(console.log);
-};
+// Degiro.getPortfolio(filepath)
+//   .then(console.log)
+//   .catch(console.log)
 
-// test(filepath);
 
-Degiro.getPortfolio(filepath)
+// Degiro.explore(`${__dirname}/resources/transactions.csv`);
+
+Degiro.getTransactions(`${__dirname}/resources/transactions.csv`)
   .then(console.log)
   .catch(console.log)
